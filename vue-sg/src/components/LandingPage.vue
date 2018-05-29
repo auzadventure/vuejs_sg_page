64b2c472e69b9c0298d41fdc34d363d9591f2111
Allan Chua <allanchua.officefiles@gmail.com>
2018-05-29 22:18:08 +0800

    -Setup initial build architecture
    -Installed Vue JS
    -Installed Vue Router
    -Installed Vue SASS Compiler
    -Installed Webpack Configuration

---
+++ vue-sg/src/components/LandingPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: "LandingPage.vue",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ vue-sg/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img src="../assets/logo.png">
+    <LandingPage msg="Welcome to VueJS Singapore Community"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LandingPage from '@/components/LandingPage.vue'
+
+export default {
+  name: 'home',
+  components: {
+    LandingPage
+  }
+}
+</script>
