e8ef4dec9bb4588585ddc8971d15e2f25c2f0441
auzadventure@gmail.com <auzadventure@gmail.com>
2018-06-02 00:06:14 +0800

    backend all 3 modules

--- ff-api/inc/JsonDB.php
+++ ff-api/inc/JsonDB.php
@@ -14,7 +14,7 @@ class JsonDB {
 	// insert 
 	public function insert(array $row) {
 		$rows = $this->findAll(true);
-		$rows[] = $row;
+		$rows = $rows + $row;
 		$this->save($rows);
 	}
 	
@@ -53,7 +53,7 @@ class JsonDB {
 		return $isDelete; 
 	}	
 	private function save($data) {
-		file_put_contents($this->filename,json_encode($data));
+		file_put_contents($this->filename,json_encode($data, JSON_PRETTY_PRINT));
 	}
 	
 	private function createEmpty() {
--- ff-api/index.php
+++ ff-api/index.php
@@ -4,8 +4,11 @@ require 'vendor/autoload.php';
 $f3 = \Base::instance();
 
 require 'inc/config.php';
+require 'inc/JsonDB.php';
 
-
+$f3->set('model_contr',new JsonDB('data/contr.json'));
+$f3->set('model_sponser',new JsonDB('data/sponser.json'));
+$f3->set('model_coy', new JsonDB('data/coy.json'));
 
 
 $f3->route('GET /',
@@ -17,26 +20,43 @@ $f3->route('GET /',
 
 
 $f3->route('GET /init', function ($f3) {
-	
-	$model= $f3->get('ContrTbl');
-	$model->name = "Lionel";
-	$model->save();
+		$model = $f3->get('model_contr');
+
+		$data = [ ['name'=>'Lionel','position'=>'scrummaster'],
+				  ['name'=>'Alan', 'position'=>'systems']];
+		$model->insert($data);
+
+		$model = $f3->get('model_sponser');
+
+		$data = [ ['name'=>'zalora'],['name'=>'sia'],['name'=>'googel']];
+		$model->insert($data);		
+		
+		$model = $f3->get('model_coy');
+
+		$data = [ ['name'=>'zalora'],['name'=>'sia'],['name'=>'googel']];
+		$model->insert($data);				
 
 });
 
-
-
-$f3->route('GET /contributors/all',
+$f3->route('GET /contributor/all',
     function($f3) {
-	   $model= $f3->get('ContrTbl');
-	   $data = $model->cast();
-	   print_r($data);
-	
+		$model = $f3->get('model_contr');
+		echo json_encode($model->findAll());
 	}
 );
 
+$f3->route('GET /sponser/all',
+    function($f3) {
+		$model = $f3->get('model_sponser');
+		echo json_encode($model->findAll());
+	}
+);
 
-
-
+$f3->route('GET /coy/all',
+    function($f3) {
+		$model = $f3->get('model_coy');
+		echo json_encode($model->findAll());
+	}
+);
 
 $f3->run();
