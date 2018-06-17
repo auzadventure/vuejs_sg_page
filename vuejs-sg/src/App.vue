e0cffca9b66d469b9d4728bcd8f0931c9d8f0019
Kevin Kwa <kev.kwa@gmail.com>
2018-06-17 17:40:56 +0800

    Add navbar markup and styles

--- vuejs-sg/src/App.vue
+++ vuejs-sg/src/App.vue
@@ -1,14 +1,20 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hr />
-    <router-link to="/">Home</router-link>
-    <router-link to="/jobs">Jobs</router-link>
-    <router-link to="/companies">Companies</router-link>
-    <router-link to="/meetups">Meetups</router-link>
-    <router-link to="/sponsors">Sponsors</router-link>
-    <router-link to="/contributors">Contributors</router-link>
-    <hr />
+  <div id="app" class="container">
+    <div class="navbar row">
+      <div class="four columns">
+        <img class="navbar-logo" src="./assets/VueSGLogo.png">
+      </div>
+      <nav class="eight columns">
+        <ul class="navbar-nav">
+          <li class="navbar-nav-item"><router-link to="/">Home</router-link></li>
+          <li class="navbar-nav-item"><router-link to="/jobs">Jobs</router-link></li>
+          <li class="navbar-nav-item"><router-link to="/companies">Companies</router-link></li>
+          <li class="navbar-nav-item"><router-link to="/meetups">Meetups</router-link></li>
+          <li class="navbar-nav-item"><router-link to="/sponsors">Sponsors</router-link></li>
+          <li class="navbar-nav-item"><router-link to="/contributors">Contributors</router-link></li>
+        </ul>
+      </nav>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -21,20 +27,43 @@ export default {
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Avenir Next", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
 }
 
 a {
-  font-weight: bold;
   color: #2c3e50;
+  text-decoration: unset;
 }
 
 a.router-link-exact-active {
   color: #42b983;
 }
+
+nav, .navbar {
+  display: flex;
+  justify-content: space-between;
+}
+
+img.navbar-logo {
+  width: 220px;
+  height: 70px;
+}
+
+ul.navbar-nav {
+  text-align: right;
+  margin: auto auto;
+  list-style: none;
+}
+
+li.navbar-nav-item {
+  display: inline-block;
+  padding-left: 25px;
+  text-transform: uppercase;
+  font-weight: normal;
+}
+
 </style>
